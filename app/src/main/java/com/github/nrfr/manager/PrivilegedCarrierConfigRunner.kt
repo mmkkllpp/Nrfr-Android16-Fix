d96ab08ff2894ba59ab5c4e20ededfac8197a2cc
mmkkllpp <mmkkllpp@users.noreply.github.com>
2026-06-10 19:32:53 +0800

    fix: use reflection for hidden APIs + fix gradle build - Use reflection to access IActivityManager, ServiceManager, UiAutomationConnection - Remove hidden API import dependencies - Add Gradle wrapper files - Update GitHub Actions workflow

--- app/src/main/java/com/github/nrfr/manager/PrivilegedCarrierConfigRunner.kt
+++ app/src/main/java/com/github/nrfr/manager/PrivilegedCarrierConfigRunner.kt
@@ -1,17 +1,12 @@
 package com.github.nrfr.manager
 
-import android.app.ActivityManager
-import android.app.IActivityManager
 import android.app.Instrumentation
-import android.app.UiAutomationConnection
 import android.content.ComponentName
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.os.Process
-import android.os.ServiceManager
-import android.system.Os
 import android.telephony.CarrierConfigManager
 import org.lsposed.hiddenapibypass.HiddenApiBypass
 import rikka.shizuku.ShizukuBinderWrapper
@@ -20,6 +15,11 @@ private const val ARG_CALLER_PID = "caller_pid"
 private const val ARG_SUB_ID = "sub_id"
 private const val ARG_CONFIG = "config"
 private const val ARG_RESET = "reset"
+
+/** @hide */
+private const val INSTR_FLAG_DISABLE_HIDDEN_API_CHECKS = 1 shl 9 // 0x200
+/** @hide */
+private const val INSTR_FLAG_NO_RESTART = 1 shl 8 // 0x100
 
 object PrivilegedCarrierConfigRunner {
     init {
@@ -37,22 +37,35 @@ object PrivilegedCarrierConfigRunner {
             }
         }
 
-        val activity = ServiceManager.getService(Context.ACTIVITY_SERVICE)
-        val activityManager = IActivityManager.Stub.asInterface(ShizukuBinderWrapper(activity))
+        val activityManager = getIActivityManager()
         val component = ComponentName(context, PrivilegedCarrierConfigInstrumentation::class.java)
-        val flags = ActivityManager.INSTR_FLAG_DISABLE_HIDDEN_API_CHECKS or
-            ActivityManager.INSTR_FLAG_NO_RESTART
+        val flags = INSTR_FLAG_DISABLE_HIDDEN_API_CHECKS or INSTR_FLAG_NO_RESTART
 
-        activityManager.startInstrumentation(
-            component,
-            null,
-            flags,
-            args,
-            null,
-            UiAutomationConnection(),
-            0,
-            null
+        // startInstrumentation(ComponentName, IProfilerInfo?, int, Bundle?, IInstrumentationWatcher?,
+        //                      IUiAutomationConnection?, int, String?)
+        val startInstrumentation = activityManager.javaClass.getMethod(
+            "startInstrumentation",
+            ComponentName::class.java,
+            android.os.IBinder::class.java,
+            Int::class.javaPrimitiveType,
+            Bundle::class.java,
+            android.app.IInstrumentationWatcher::class.java,
+            Class.forName("android.app.IUiAutomationConnection"),
+            Int::class.javaPrimitiveType,
+            String::class.java
         )
+        val uiAutomationConnection = Class.forName("android.app.UiAutomationConnection").newInstance()
+        startInstrumentation.invoke(activityManager, component, null, flags, args, null, uiAutomationConnection, 0, null)
+    }
+
+    private fun getIActivityManager(): Any {
+        val serviceManagerClass = Class.forName("android.os.ServiceManager")
+        val getService = serviceManagerClass.getMethod("getService", String::class.java)
+        val binder = getService.invoke(null, Context.ACTIVITY_SERVICE)
+        val iActivityManagerClass = Class.forName("android.app.IActivityManager")
+        val asInterface = iActivityManagerClass.getMethod("asInterface", android.os.IBinder::class.java)
+        val am = asInterface.invoke(null, ShizukuBinderWrapper(binder as android.os.IBinder))
+        return am
     }
 }
 
@@ -70,19 +83,25 @@ class PrivilegedCarrierConfigInstrumentation : Instrumentation() {
             return
         }
 
-        val activity = ServiceManager.getService(Context.ACTIVITY_SERVICE)
-        val activityManager = IActivityManager.Stub.asInterface(ShizukuBinderWrapper(activity))
-
+        val activityManager = getIActivityManager()
         val subId = arguments.getInt(ARG_SUB_ID)
         val bundle = getPersistableBundle(arguments)
 
         try {
-            activityManager.startDelegateShellPermissionIdentity(Os.getuid(), null)
+            // startDelegateShellPermissionIdentity(int uid, String[]? permissions)
+            val delegateMethod = activityManager.javaClass.getMethod(
+                "startDelegateShellPermissionIdentity",
+                Int::class.javaPrimitiveType,
+                Array<String>::class.java
+            )
+            delegateMethod.invoke(activityManager, Process.myUid(), null)
             overrideCarrierConfig(subId, bundle, persistent = true)
         } catch (e: SecurityException) {
             overrideCarrierConfig(subId, bundle, persistent = false)
         } finally {
-            activityManager.stopDelegateShellPermissionIdentity()
+            try {
+                activityManager.javaClass.getMethod("stopDelegateShellPermissionIdentity").invoke(activityManager)
+            } catch (_: Exception) {}
             finish(0, Bundle())
         }
     }
@@ -92,7 +111,9 @@ class PrivilegedCarrierConfigInstrumentation : Instrumentation() {
         bundle: PersistableBundle?,
         persistent: Boolean
     ) {
-        val manager = targetContext.getSystemService(CarrierConfigManager::class.java)
+        @Suppress("UNCHECKED_CAST")
+        val manager = targetContext.getSystemService(Context.CARRIER_CONFIG_SERVICE) as? CarrierConfigManager
+            ?: return
         manager.overrideConfig(subId, bundle, persistent)
     }
 
@@ -100,12 +121,21 @@ class PrivilegedCarrierConfigInstrumentation : Instrumentation() {
         if (arguments.getBoolean(ARG_RESET)) {
             return null
         }
-
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             arguments.getParcelable(ARG_CONFIG, PersistableBundle::class.java)
         } else {
             @Suppress("DEPRECATION")
             arguments.getParcelable<PersistableBundle>(ARG_CONFIG)
         }
+    }
+
+    private fun getIActivityManager(): Any {
+        val serviceManagerClass = Class.forName("android.os.ServiceManager")
+        val getService = serviceManagerClass.getMethod("getService", String::class.java)
+        val binder = getService.invoke(null, Context.ACTIVITY_SERVICE)
+        val iActivityManagerClass = Class.forName("android.app.IActivityManager")
+        val asInterface = iActivityManagerClass.getMethod("asInterface", android.os.IBinder::class.java)
+        val am = asInterface.invoke(null, ShizukuBinderWrapper(binder as android.os.IBinder))
+        return am
     }
 }
